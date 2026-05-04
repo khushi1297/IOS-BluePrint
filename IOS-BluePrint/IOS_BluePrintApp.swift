a615dab273fe12bf9d0bed4b3b571a538f4895a6
Jaju <Khushi.Arya@student.uts.edu.au>
2026-05-04 15:55:37 +1000

    Initial Commit

---
+++ IOS-BluePrint/IOS_BluePrintApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_BluePrintApp.swift
+//  IOS-BluePrint
+//
+//  Created by Khush on 04/05/26.
+//
+
+import SwiftUI
+
+@main
+struct IOS_BluePrintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
